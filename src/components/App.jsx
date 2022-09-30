af17de6108171c146228bc6cc801b4b610a0efaa
Vitalii Boiko <107396176+vitaliiseeker@users.noreply.github.com>
2022-10-01 01:42:53 +0300

    update code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import user from "../data/user.json";
-import { Profile } from "./Profile/Profile.js";
+import { Profile } from "components/Profile/Profile";
 
 export const App = () => {
   return (
